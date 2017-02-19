8b51ecc07746f63416e38f7df4f8add1bdc7ae14
Levente Santha <lsantha@jnode.ro>
2017-02-19 16:40:40 +0200

    no display

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm /tmp/com1.txt
-qemu-system-x86_64 -cdrom jnode-x86-lite.iso -m 1024 -cpu pentium -serial file:/tmp/com1.txt&
+qemu-system-x86_64 -cdrom jnode-x86-lite.iso -m 1024 -cpu pentium -serial file:/tmp/com1.txt -display none&
 pid=$!
 sleep 1
 tail -f /tmp/com1.txt&
