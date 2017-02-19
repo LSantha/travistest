5edd5fc67152aea8f385eb6d3446b8099055aea7
Levente Santha <lsantha@jnode.ro>
2017-02-19 21:22:21 +0200

    run improved

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 rm /tmp/com1.txt
-qemu-system-x86_64 -cdrom jnode-x86-lite.iso -m 1024 -cpu pentium -serial file:/tmp/com1.txt -display none&
-pid=$!
+echo "Starting JNode in QEMU"
+qemu-system-x86_64 -cdrom jnode-x86-lite.iso -m 1024 -M pc -cpu pentium -boot once=d,menu=off -net none -serial file:/tmp/com1.txt -display none&
+qemu_pid=$!
 sleep 1
 tail -f /tmp/com1.txt&
+tail_pid=$!
 until grep RAMFS /tmp/com1.txt; do
 sleep 1
 done
-kill $pid
+echo "Stopping QEMU"
+kill $qemu_pid
+kill $tail_pid
